4ce972c21e48f6781b97d4407204efa18294055d
Adam Kupczyk <akupczyk@ibm.com>
2023-11-14 16:25:01 +0000

    os/bluestore: Refactor of write path. New punch_hole_2 function.
    Introducing new logic of Onode processing during write.
    New punch_hole_2 function empties range, but keeps track of elements:
    - allocations that are no longer used
    - blobs that are now empty
    - shared blobs that got modified
    - statfs changes to apply later
    
    This change allows to reuse allocation for deferred freely, which means
    that we can use allocations in deferred mode in other blob then they come from.
    
    Signed-off-by: Adam Kupczyk <akupczyk@ibm.com>

--- src/os/bluestore/BlueStore.h
+++ src/os/bluestore/BlueStore.h
@@ -785,6 +785,11 @@ public:
 		 PExtentVector *r);
     // update caches to reflect content up to seq
     void finish_write(uint64_t seq);
+    uint32_t put_ref_accumulate(
+      Collection *coll,
+      uint32_t offset,
+      uint32_t length,
+      PExtentVector *released_disk);
     /// split the blob
     void split(Collection *coll, uint32_t blob_offset, Blob *o);
 
@@ -3621,6 +3626,15 @@ private:
       uint64_t loffs_end,
       uint64_t min_alloc_size);
   };
+  BlueStore::extent_map_t::iterator _punch_hole_2(
+    Collection* c,
+    OnodeRef& o,
+    uint32_t offset,
+    uint32_t length,
+    PExtentVector& released,
+    std::vector<BlobRef>& pruned_blobs,
+    std::set<SharedBlobRef>& shared_changed,
+    volatile_statfs& statfs_delta);
   void _do_write_small(
     TransContext *txc,
     CollectionRef &c,
