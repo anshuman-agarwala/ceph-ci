283eb2be2b1cf8697cfd85ecc473a65e5254afe3
Kaleb S. KEITHLEY <kkeithle@redhat.com>
2024-03-21 08:07:17 -0400

    Adds (shared) libs:     * rgw_sal_rados (librgw_sal_rados) rados plugin     * rgw_sal_d3n (librgw_sal_d3n) d3n plugin     * rgw_sal_motr (librgw_sal_motr) motr plugin     * rgw_sal_daos (librgw_sal_daos) daos plugin     * rgw_sal_dbstore (librgw_sal_dbstore) dbstore plugin     * rados_sal library (librados_sal), below the line shlib for rgw_sal_rados and rgw_sal_d3n
        future:
        * rados_sal sources move to a subdir, move rados_sal (sources) to the
        rados plugin and remove it as a lib, or make it a shlib
        * convert (static) librgw_common to a shared lib
        * filters as plugins(?)
    
    Signed-off-by: Kaleb S. KEITHLEY <kkeithle@redhat.com>

--- src/test/test_rgw_admin_log.cc
+++ src/test/test_rgw_admin_log.cc
@@ -1559,6 +1559,12 @@ TEST(TestRGWAdmin, bilog_trim) {
   ASSERT_EQ(0, user_rm(uid, display_name));
 }
 
+class RGWCurlHandles;
+RGWCurlHandles *handles = nullptr;
+
+class RGWHTTPManager;
+RGWHTTPManager *rgw_http_manager = nullptr;
+
 int main(int argc, char *argv[]){
   auto args = argv_to_vec(argc, argv);
 
