ceph/ceph

test/crimson/seastore/test_object_data_handler: disable max_extent_size for remap related cases
Signed-off-by: Xuehan Xu <xuxuehan@qianxin.com>

src/test/crimson/seastore/test_object_data_handler.cc
@@ -245,6 +245,17 @@ struct object_data_handler_test_t:
     crimson::common::local_conf().set_val("seastore_data_delta_based_overwrite", "0").get();
   }
 
+  void disable_max_extent_size() {
+    cache->set_max_extent_size(16777216);
+    crimson::common::local_conf().set_val(
+      "seastore_max_extent_size", "16777216").get();
+  }
+  void enable_max_extent_size() {
+    cache->set_max_extent_size(8192);
+    crimson::common::local_conf().set_val(
+      "seastore_max_extent_size", "8192").get();
+  }
+
   laddr_t get_random_laddr(size_t block_size, laddr_t limit) {
     return block_size *
       std::uniform_int_distribution<>(0, (limit / block_size) - 1)(gen);
@@ -568,6 +579,7 @@ TEST_P(object_data_handler_test_t, no_overwrite) {
 
 TEST_P(object_data_handler_test_t, remap_left) {
   run_async([this] {
+    disable_max_extent_size();
     write_right();
 
     auto pins = get_mappings(0, 128<<10);
@@ -581,11 +593,13 @@ TEST_P(object_data_handler_test_t, remap_left) {
       i++;
     }
     read(0, 128<<10);
+    enable_max_extent_size();
   });
 }
 
 TEST_P(object_data_handler_test_t, overwrite_right) {
   run_async([this] {
+    disable_max_extent_size();
     set_overwrite_threshold();
     write_right();
 
@@ -593,11 +607,13 @@ TEST_P(object_data_handler_test_t, overwrite_right) {
     EXPECT_EQ(pins.size(), 1);
     read(0, 128<<10);
     unset_overwrite_threshold();
+    enable_max_extent_size();
   });
 }
 
 TEST_P(object_data_handler_test_t, remap_right) {
   run_async([this] {
+    disable_max_extent_size();
     write_left();
 
     auto pins = get_mappings(0, 128<<10);
@@ -611,22 +627,26 @@ TEST_P(object_data_handler_test_t, remap_right) {
       i++;
     }
     read(0, 128<<10);
+    enable_max_extent_size();
   });
 }
 
 TEST_P(object_data_handler_test_t, overwrite_left) {
   run_async([this] {
+    disable_max_extent_size();
     set_overwrite_threshold();
     write_left();
     auto pins = get_mappings(0, 128<<10);
     EXPECT_EQ(pins.size(), 1);
     read(0, 128<<10);
     unset_overwrite_threshold();
+    enable_max_extent_size();
   });
 }
 
 TEST_P(object_data_handler_test_t, remap_right_left) {
   run_async([this] {
+    disable_max_extent_size();
     write_right_left();
 
     auto pins = get_mappings(0, 128<<10);
@@ -639,22 +659,26 @@ TEST_P(object_data_handler_test_t, remap_right_left) {
       EXPECT_EQ(pin->get_key() - base, res[i]);
       i++;
     }
+    enable_max_extent_size();
   });
 }
 
 TEST_P(object_data_handler_test_t, overwrite_right_left) {
   run_async([this] {
+    disable_max_extent_size();
     set_overwrite_threshold();
     write_right_left();
     auto pins = get_mappings(0, 128<<10);
     EXPECT_EQ(pins.size(), 1);
     read(0, 128<<10);
     unset_overwrite_threshold();
+    enable_max_extent_size();
   });
 }
 
 TEST_P(object_data_handler_test_t, multiple_remap) {
   run_async([this] {
+    disable_max_extent_size();
     multiple_write();
     auto pins = get_mappings(0, 128<<10);
     EXPECT_EQ(pins.size(), 10);
@@ -668,17 +692,20 @@ TEST_P(object_data_handler_test_t, multiple_remap) {
       i++;
     }
     read(0, 128<<10);
+    enable_max_extent_size();
   });
 }
 
 TEST_P(object_data_handler_test_t, multiple_overwrite) {
   run_async([this] {
+    disable_max_extent_size();
     set_overwrite_threshold();
     multiple_write();
     auto pins = get_mappings(0, 128<<10);
     EXPECT_EQ(pins.size(), 1);
     read(0, 128<<10);
     unset_overwrite_threshold();
+    enable_max_extent_size();
   });
 }
 

anshuman-agarwala/ceph-ci

src/test/crimson/seastore/test_object_data_handler.cc
@@ -245,6 +245,17 @@ struct object_data_handler_test_t:
     crimson::common::local_conf().set_val("seastore_data_delta_based_overwrite", "0").get();
   }
 
+  void disable_max_extent_size() {
+    epm->set_max_extent_size(16777216);
+    crimson::common::local_conf().set_val(
+      "seastore_max_data_allocation_size", "16777216").get();
+  }
+  void enable_max_extent_size() {
+    epm->set_max_extent_size(8192);
+    crimson::common::local_conf().set_val(
+      "seastore_max_data_allocation_size", "8192").get();
+  }
+
   laddr_t get_random_laddr(size_t block_size, laddr_t limit) {
     return block_size *
       std::uniform_int_distribution<>(0, (limit / block_size) - 1)(gen);
@@ -568,6 +579,7 @@ TEST_P(object_data_handler_test_t, no_overwrite) {
 
 TEST_P(object_data_handler_test_t, remap_left) {
   run_async([this] {
+    disable_max_extent_size();
     write_right();
 
     auto pins = get_mappings(0, 128<<10);
@@ -581,11 +593,13 @@ TEST_P(object_data_handler_test_t, remap_left) {
       i++;
     }
     read(0, 128<<10);
+    enable_max_extent_size();
   });
 }
 
 TEST_P(object_data_handler_test_t, overwrite_right) {
   run_async([this] {
+    disable_max_extent_size();
     set_overwrite_threshold();
     write_right();
 
@@ -593,11 +607,13 @@ TEST_P(object_data_handler_test_t, overwrite_right) {
     EXPECT_EQ(pins.size(), 1);
     read(0, 128<<10);
     unset_overwrite_threshold();
+    enable_max_extent_size();
   });
 }
 
 TEST_P(object_data_handler_test_t, remap_right) {
   run_async([this] {
+    disable_max_extent_size();
     write_left();
 
     auto pins = get_mappings(0, 128<<10);
@@ -611,22 +627,26 @@ TEST_P(object_data_handler_test_t, remap_right) {
       i++;
     }
     read(0, 128<<10);
+    enable_max_extent_size();
   });
 }
 
 TEST_P(object_data_handler_test_t, overwrite_left) {
   run_async([this] {
+    disable_max_extent_size();
     set_overwrite_threshold();
     write_left();
     auto pins = get_mappings(0, 128<<10);
     EXPECT_EQ(pins.size(), 1);
     read(0, 128<<10);
     unset_overwrite_threshold();
+    enable_max_extent_size();
   });
 }
 
 TEST_P(object_data_handler_test_t, remap_right_left) {
   run_async([this] {
+    disable_max_extent_size();
     write_right_left();
 
     auto pins = get_mappings(0, 128<<10);
@@ -639,22 +659,26 @@ TEST_P(object_data_handler_test_t, remap_right_left) {
       EXPECT_EQ(pin->get_key() - base, res[i]);
       i++;
     }
+    enable_max_extent_size();
   });
 }
 
 TEST_P(object_data_handler_test_t, overwrite_right_left) {
   run_async([this] {
+    disable_max_extent_size();
     set_overwrite_threshold();
     write_right_left();
     auto pins = get_mappings(0, 128<<10);
     EXPECT_EQ(pins.size(), 1);
     read(0, 128<<10);
     unset_overwrite_threshold();
+    enable_max_extent_size();
   });
 }
 
 TEST_P(object_data_handler_test_t, multiple_remap) {
   run_async([this] {
+    disable_max_extent_size();
     multiple_write();
     auto pins = get_mappings(0, 128<<10);
     EXPECT_EQ(pins.size(), 3);
@@ -667,17 +691,20 @@ TEST_P(object_data_handler_test_t, multiple_remap) {
       i++;
     }
     read(0, 128<<10);
+    enable_max_extent_size();
   });
 }
 
 TEST_P(object_data_handler_test_t, multiple_overwrite) {
   run_async([this] {
+    disable_max_extent_size();
     set_overwrite_threshold();
     multiple_write();
     auto pins = get_mappings(0, 128<<10);
     EXPECT_EQ(pins.size(), 1);
     read(0, 128<<10);
     unset_overwrite_threshold();
+    enable_max_extent_size();
   });
 }
 