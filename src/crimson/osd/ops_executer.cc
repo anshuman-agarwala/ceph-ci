a8289204e5331844288010a24482cb83367f999b
Matan Breizman <mbreizma@redhat.com>
2024-05-01 15:11:59 +0000

    wip
    Signed-off-by: Matan Breizman <mbreizma@redhat.com>

--- src/crimson/osd/ops_executer.cc
+++ src/crimson/osd/ops_executer.cc
@@ -805,7 +805,7 @@ void OpsExecuter::fill_op_params(OpsExecuter::modified_by m)
 {
   osd_op_params->req_id = msg->get_reqid();
   osd_op_params->mtime = msg->get_mtime();
-  osd_op_params->at_version = pg->get_cur_version();
+  osd_op_params->at_version = pg->get_next_version();
   osd_op_params->pg_trim_to = pg->get_pg_trim_to();
   osd_op_params->min_last_complete_ondisk = pg->get_min_last_complete_ondisk();
   osd_op_params->last_complete = pg->get_info().last_complete;
