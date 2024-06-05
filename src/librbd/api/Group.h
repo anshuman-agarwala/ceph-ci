ceph/ceph

rbd-mirror: add undo code, exclusive locking and quiescing
* add essential logic to undo partially succeeded API's like, group promote,
  group demote, group enable, group disable, group image add and
  group image remove
* add exclusive locking and quiescing with-in all the required group API's
* adress code duplication and optimization with in the group API's

Signed-off-by: Prasanna Kumar Kalever <prasanna.kalever@redhat.com>

src/librbd/api/Group.h
@@ -19,7 +19,8 @@ template <typename ImageCtxT = librbd::ImageCtx>
 struct Group {
 
   static int create(librados::IoCtx& io_ctx, const char *group_name);
-  static int remove(librados::IoCtx& io_ctx, const char *group_name);
+  static int remove(librados::IoCtx& io_ctx,
+                    const char *group_name, uint32_t flags);
   static int list(librados::IoCtx& io_ctx, std::vector<std::string> *names);
   static int list(librados::IoCtx& io_ctx,
                   std::map<std::string, std::string> *name_to_id_map);
@@ -27,13 +28,16 @@ struct Group {
                     const char *dest_group_name);
 
   static int image_add(librados::IoCtx& group_ioctx, const char *group_name,
-		       librados::IoCtx& image_ioctx, const char *image_name);
+                       librados::IoCtx& image_ioctx, const char *image_name,
+                       uint32_t flags);
   static int image_remove(librados::IoCtx& group_ioctx, const char *group_name,
-		          librados::IoCtx& image_ioctx, const char *image_name);
+                          librados::IoCtx& image_ioctx, const char *image_name,
+                          uint32_t flags);
   static int image_remove_by_id(librados::IoCtx& group_ioctx,
                                 const char *group_name,
                                 librados::IoCtx& image_ioctx,
-                                const char *image_id);
+                                const char *image_id,
+                                uint32_t flags);
   static int image_list(librados::IoCtx& group_ioctx, const char *group_name,
 		        std::vector<group_image_info_t> *images);
 

anshuman-agarwala/ceph-ci

src/librbd/api/Group.h
@@ -28,13 +28,16 @@ struct Group {
                     const char *dest_group_name);
 
   static int image_add(librados::IoCtx& group_ioctx, const char *group_name,
-		       librados::IoCtx& image_ioctx, const char *image_name);
+                       librados::IoCtx& image_ioctx, const char *image_name,
+                       uint32_t flags);
   static int image_remove(librados::IoCtx& group_ioctx, const char *group_name,
-		          librados::IoCtx& image_ioctx, const char *image_name);
+                          librados::IoCtx& image_ioctx, const char *image_name,
+                          uint32_t flags);
   static int image_remove_by_id(librados::IoCtx& group_ioctx,
                                 const char *group_name,
                                 librados::IoCtx& image_ioctx,
-                                const char *image_id);
+                                const char *image_id,
+                                uint32_t flags);
   static int image_list(librados::IoCtx& group_ioctx, const char *group_name,
 		        std::vector<group_image_info_t> *images);
 
@@ -56,8 +59,12 @@ struct Group {
   static int group_image_list_by_id(librados::IoCtx& group_ioctx,
                                     const std::string &group_id,
                                     std::vector<cls::rbd::GroupImageStatus> *images);
-  static int group_image_remove(librados::IoCtx& group_ioctx, std::string group_id,
-                                librados::IoCtx& image_ioctx, std::string image_id);
+  static int group_image_remove(librados::IoCtx& group_ioctx,
+                                std::string group_id,
+                                librados::IoCtx& image_ioctx,
+                                std::string image_id,
+                                bool resync,
+                                uint32_t flags);
 
 };
 