af108d963c48181b05e007898751d0b35249b829
Redouane Kachach <rkachach@ibm.com>
2024-06-24 17:51:33 +0200

    mgr/cephadm: removing changes that have been moved to other PR
    Signed-off-by: Redouane Kachach <rkachach@ibm.com>

--- src/pybind/mgr/cephadm/tests/test_services.py
+++ src/pybind/mgr/cephadm/tests/test_services.py
@@ -1426,6 +1426,7 @@ spec:
                             "deploy_arguments": [],
                             "params": {
                                 'tcp_ports': [4200, 9094],
+                                'reconfig': True
                             },
                             "meta": {
                                 'service_name': 'alertmanager',
@@ -1439,7 +1440,7 @@ spec:
                             },
                             "config_blobs": {},
                         }),
-                        use_current_daemon_image=False,
+                        use_current_daemon_image=True,
                     )
 
 
