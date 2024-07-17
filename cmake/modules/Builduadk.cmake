775ef028212a32d586e568a8af365d4284fa8c46
Rongqi Sun <sunrongqi@huawei.com>
2024-07-17 17:03:55 +0800

    uadk/build: make it as a static library
    Signed-off-by: Rongqi Sun <sunrongqi@huawei.com>

--- cmake/modules/Builduadk.cmake
+++ cmake/modules/Builduadk.cmake
@@ -2,10 +2,11 @@ function(build_uadk)
     set(UADK_INSTALL_DIR ${CMAKE_BINARY_DIR}/src/uadk/install)
     set(UADK_INCLUDE_DIR ${UADK_INSTALL_DIR}/include)
     set(UADK_LIBRARY_DIR ${UADK_INSTALL_DIR}/lib)
-    set(UADK_WD_LIBRARY ${UADK_LIBRARY_DIR}/libwd.so)
-    set(UADK_WD_COMP_LIBRARY ${UADK_LIBRARY_DIR}/libwd_comp.so)
+    set(UADK_WD_LIBRARY ${UADK_LIBRARY_DIR}/libwd.a)
+    set(UADK_WD_COMP_LIBRARY ${UADK_LIBRARY_DIR}/libwd_comp.a)
+    set(UADK_WD_ZIP_LIBRARY ${UADK_LIBRARY_DIR}/uadk/libhisi_zip.a)
     set(configure_cmd env ./configure --prefix=${UADK_INSTALL_DIR})
-    list(APPEND configure_cmd --with-pic --enable-shared --disable-static)
+    list(APPEND configure_cmd --with-pic --enable-static --disable-shared --with-static_drv)
 
     include(ExternalProject)
     ExternalProject_Add(uadk_ext
@@ -19,7 +20,7 @@ function(build_uadk)
             CMAKE_ARGS -DCMAKE_CXX_COMPILER=which g++
             CONFIGURE_COMMAND ./autogen.sh COMMAND ${configure_cmd}
             BUILD_COMMAND make
-	    BUILD_BYPRODUCTS ${UADK_WD_LIBRARY} ${UADK_WD_COMP_LIBRARY}
+	    BUILD_BYPRODUCTS ${UADK_WD_LIBRARY} ${UADK_WD_COMP_LIBRARY} ${UADK_WD_ZIP_LIBRARY}
             INSTALL_COMMAND make install
             LOG_CONFIGURE ON
             LOG_BUILD ON
@@ -29,13 +30,13 @@ function(build_uadk)
 
     ExternalProject_Get_Property(uadk_ext source_dir)
     set(UADK_INCLUDE_DIR ${UADK_INCLUDE_DIR} PARENT_SCOPE)
-    set(UADK_WD_COMP_LIBRARY ${UADK_LIBRARIES} PARENT_SCOPE)
-    set(UADK_WD_LIBRARY ${UADK_WD_LIBRARIES} PARENT_SCOPE)
 
     add_library(uadk::uadk UNKNOWN IMPORTED)
     add_library(uadk::uadkwd UNKNOWN IMPORTED)
+    add_library(uadk::uadkzip UNKNOWN IMPORTED)
     add_dependencies(uadk::uadk uadk_ext)
     add_dependencies(uadk::uadkwd uadk_ext)
+    add_dependencies(uadk::uadkzip uadk_ext)
     file(MAKE_DIRECTORY ${UADK_INCLUDE_DIR})
     set_target_properties(uadk::uadk PROPERTIES
         INTERFACE_INCLUDE_DIRECTORIES ${UADK_INCLUDE_DIR}
@@ -45,4 +46,8 @@ function(build_uadk)
         INTERFACE_INCLUDE_DIRECTORIES ${UADK_INCLUDE_DIR}
         IMPORTED_LINK_INTERFACE_LANGUAGES "C"
 	IMPORTED_LOCATION "${UADK_WD_LIBRARY}")
+    set_target_properties(uadk::uadkzip PROPERTIES
+        INTERFACE_INCLUDE_DIRECTORIES ${UADK_INCLUDE_DIR}
+        IMPORTED_LINK_INTERFACE_LANGUAGES "C"
+	IMPORTED_LOCATION "${UADK_WD_ZIP_LIBRARY}")
 endfunction()
