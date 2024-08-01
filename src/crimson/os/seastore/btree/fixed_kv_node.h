ceph/ceph

crimson/os/seastore/cached_extent: add the "refresh" ability to lba mappings
So that we don't have to re-iterate through the whole lba tree to get
a "parent-viewable" mapping in most cases.

Signed-off-by: Xuehan Xu <xuxuehan@qianxin.com>

src/crimson/os/seastore/btree/fixed_kv_node.h
@@ -125,6 +125,27 @@ struct FixedKVNode : ChildableCachedExtent {
     copy_dests.dests.erase(dest);
   }
 
+  FixedKVNodeRef find_dest(Transaction &t, node_key_t key) {
+    auto mut_iter = mutation_pendings.find(
+      t.get_trans_id(), trans_spec_view_t::cmp_t());
+    if (mut_iter != mutation_pendings.end()) {
+      assert(copy_dests_by_trans.find(t.get_trans_id()) ==
+	copy_dests_by_trans.end());
+      return (FixedKVNode*)(&(*mut_iter));
+    }
+    auto iter = copy_dests_by_trans.find(
+      t.get_trans_id(), trans_spec_view_t::cmp_t());
+    ceph_assert(iter != copy_dests_by_trans.end());
+    auto &copy_dests = static_cast<copy_dests_t&>(*iter);
+    auto it = copy_dests.dests.lower_bound(key);
+    if ((*it)->range.begin > key) {
+      ceph_assert(it != copy_dests.dests.begin());
+      --it;
+    }
+    ceph_assert((*it)->range.begin <= key && key < (*it)->range.end);
+    return *it;
+  }
+
   bool is_linked() {
     assert(!has_parent_tracker() || !(bool)root_block);
     return (bool)has_parent_tracker() || (bool)root_block;

src/crimson/os/seastore/cached_extent.h
@@ -1121,6 +1121,10 @@ public:
   virtual val_t get_val() const = 0;
   virtual key_t get_key() const = 0;
   virtual PhysicalNodeMappingRef<key_t, val_t> duplicate() const = 0;
+  virtual PhysicalNodeMappingRef<key_t, val_t> refresh() {
+    ceph_abort("impossible");
+    return {};
+  }
   virtual bool has_been_invalidated() const = 0;
   virtual CachedExtentRef get_parent() const = 0;
   virtual uint16_t get_pos() const = 0;
@@ -1155,6 +1159,7 @@ public:
     return !get_val().is_real();
   }
   virtual bool is_parent_viewable() const = 0;
+  virtual bool is_parent_valid() const = 0;
   virtual bool parent_modified() const {
     ceph_abort("impossible");
     return false;

src/crimson/os/seastore/lba_manager/btree/btree_lba_manager.h
@@ -176,6 +176,14 @@ public:
     auto &p = static_cast<LBALeafNode&>(*parent);
     p.maybe_fix_mapping_pos(*this);
   }
+
+  LBAMappingRef refresh() final {
+    assert(is_parent_valid() && !is_parent_viewable());
+    auto &p = static_cast<LBALeafNode&>(*parent);
+    auto &viewable_p = static_cast<LBALeafNode&>(
+      *p.find_dest(ctx.trans, get_key()));
+    return viewable_p.get_mapping(ctx, get_key());
+  }
 protected:
   std::unique_ptr<BtreeNodeMapping<laddr_t, paddr_t>> _duplicate(
     op_context_t<laddr_t> ctx) const final {

src/crimson/os/seastore/transaction_manager.h
@@ -183,11 +183,16 @@ public:
   {
     auto fut = base_iertr::make_ready_future<LBAMappingRef>();
     if (!pin->is_parent_viewable()) {
-      fut = get_pin(t, pin->get_key()
-      ).handle_error_interruptible(
-	crimson::ct_error::enoent::assert_failure{"unexpected enoent"},
-	crimson::ct_error::input_output_error::pass_further{}
-      );
+      if (pin->is_parent_valid()) {
+	pin = pin->refresh();
+	fut = base_iertr::make_ready_future<LBAMappingRef>(std::move(pin));
+      } else {
+	fut = get_pin(t, pin->get_key()
+	).handle_error_interruptible(
+	  crimson::ct_error::enoent::assert_failure{"unexpected enoent"},
+	  crimson::ct_error::input_output_error::pass_further{}
+	);
+      }
     } else {
       pin->maybe_fix_pos();
       fut = base_iertr::make_ready_future<LBAMappingRef>(std::move(pin));
@@ -475,15 +480,19 @@ public:
       auto fut = base_iertr::now();
       if (!pin->is_indirect()) {
 	if (!pin->is_parent_viewable()) {
-	  fut = get_pin(t, pin->get_key()
-	  ).si_then([&pin](auto npin) {
-	    assert(npin);
-	    pin = std::move(npin);
-	    return seastar::now();
-	  }).handle_error_interruptible(
-	    crimson::ct_error::enoent::assert_failure{"unexpected enoent"},
-	    crimson::ct_error::input_output_error::pass_further{}
-	  );
+	  if (pin->is_parent_valid()) {
+	    pin = pin->refresh();
+	  } else {
+	    fut = get_pin(t, pin->get_key()
+	    ).si_then([&pin](auto npin) {
+	      assert(npin);
+	      pin = std::move(npin);
+	      return seastar::now();
+	    }).handle_error_interruptible(
+	      crimson::ct_error::enoent::assert_failure{"unexpected enoent"},
+	      crimson::ct_error::input_output_error::pass_further{}
+	    );
+	  }
 	} else {
 	  pin->maybe_fix_pos();
 	}

anshuman-agarwala/ceph-ci

src/crimson/os/seastore/btree/fixed_kv_node.h
@@ -134,6 +134,28 @@ struct FixedKVNode : ChildableCachedExtent {
     copy_dests.dests_by_key.erase(dest);
   }
 
+  FixedKVNodeRef find_pending_version(Transaction &t, node_key_t key) {
+    assert(is_stable());
+    auto mut_iter = mutation_pendings.find(
+      t.get_trans_id(), trans_spec_view_t::cmp_t());
+    if (mut_iter != mutation_pendings.end()) {
+      assert(copy_dests_by_trans.find(t.get_trans_id()) ==
+	copy_dests_by_trans.end());
+      return (FixedKVNode*)(&(*mut_iter));
+    }
+    auto iter = copy_dests_by_trans.find(
+      t.get_trans_id(), trans_spec_view_t::cmp_t());
+    ceph_assert(iter != copy_dests_by_trans.end());
+    auto &copy_dests = static_cast<copy_dests_t&>(*iter);
+    auto it = copy_dests.dests_by_key.lower_bound(key);
+    if ((*it)->range.begin > key) {
+      ceph_assert(it != copy_dests.dests_by_key.begin());
+      --it;
+    }
+    ceph_assert((*it)->range.begin <= key && key < (*it)->range.end);
+    return *it;
+  }
+
   bool is_linked() {
     assert(!has_parent_tracker() || !(bool)root_block);
     return (bool)has_parent_tracker() || (bool)root_block;

src/crimson/os/seastore/cached_extent.h
@@ -1121,6 +1121,10 @@ public:
   virtual val_t get_val() const = 0;
   virtual key_t get_key() const = 0;
   virtual PhysicalNodeMappingRef<key_t, val_t> duplicate() const = 0;
+  virtual PhysicalNodeMappingRef<key_t, val_t> refresh_with_pending_parent() {
+    ceph_abort("impossible");
+    return {};
+  }
   virtual bool has_been_invalidated() const = 0;
   virtual CachedExtentRef get_parent() const = 0;
   virtual uint16_t get_pos() const = 0;
@@ -1155,6 +1159,7 @@ public:
     return !get_val().is_real();
   }
   virtual bool is_parent_viewable() const = 0;
+  virtual bool is_parent_valid() const = 0;
   virtual bool parent_modified() const {
     ceph_abort("impossible");
     return false;

src/crimson/os/seastore/lba_manager/btree/btree_lba_manager.h
@@ -176,6 +176,14 @@ public:
     auto &p = static_cast<LBALeafNode&>(*parent);
     p.maybe_fix_mapping_pos(*this);
   }
+
+  LBAMappingRef refresh_with_pending_parent() final {
+    assert(is_parent_valid() && !is_parent_viewable());
+    auto &p = static_cast<LBALeafNode&>(*parent);
+    auto &viewable_p = static_cast<LBALeafNode&>(
+      *p.find_pending_version(ctx.trans, get_key()));
+    return viewable_p.get_mapping(ctx, get_key());
+  }
 protected:
   std::unique_ptr<BtreeNodeMapping<laddr_t, paddr_t>> _duplicate(
     op_context_t<laddr_t> ctx) const final {

src/crimson/os/seastore/transaction_manager.h
@@ -184,11 +184,16 @@ public:
   {
     auto fut = base_iertr::make_ready_future<LBAMappingRef>();
     if (!pin->is_parent_viewable()) {
-      fut = get_pin(t, pin->get_key()
-      ).handle_error_interruptible(
-	crimson::ct_error::enoent::assert_failure{"unexpected enoent"},
-	crimson::ct_error::input_output_error::pass_further{}
-      );
+      if (pin->is_parent_valid()) {
+	pin = pin->refresh_with_pending_parent();
+	fut = base_iertr::make_ready_future<LBAMappingRef>(std::move(pin));
+      } else {
+	fut = get_pin(t, pin->get_key()
+	).handle_error_interruptible(
+	  crimson::ct_error::enoent::assert_failure{"unexpected enoent"},
+	  crimson::ct_error::input_output_error::pass_further{}
+	);
+      }
     } else {
       pin->maybe_fix_pos();
       fut = base_iertr::make_ready_future<LBAMappingRef>(std::move(pin));
@@ -476,15 +481,19 @@ public:
       auto fut = base_iertr::now();
       if (!pin->is_indirect()) {
 	if (!pin->is_parent_viewable()) {
-	  fut = get_pin(t, pin->get_key()
-	  ).si_then([&pin](auto npin) {
-	    assert(npin);
-	    pin = std::move(npin);
-	    return seastar::now();
-	  }).handle_error_interruptible(
-	    crimson::ct_error::enoent::assert_failure{"unexpected enoent"},
-	    crimson::ct_error::input_output_error::pass_further{}
-	  );
+	  if (pin->is_parent_valid()) {
+	    pin = pin->refresh_with_pending_parent();
+	  } else {
+	    fut = get_pin(t, pin->get_key()
+	    ).si_then([&pin](auto npin) {
+	      assert(npin);
+	      pin = std::move(npin);
+	      return seastar::now();
+	    }).handle_error_interruptible(
+	      crimson::ct_error::enoent::assert_failure{"unexpected enoent"},
+	      crimson::ct_error::input_output_error::pass_further{}
+	    );
+	  }
 	} else {
 	  pin->maybe_fix_pos();
 	}