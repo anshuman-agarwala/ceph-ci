zhscn/ceph

crimson/osd: clear ondisk temp objects on startup
Signed-off-by: Xuehan Xu <xuxuehan@qianxin.com>

src/crimson/osd/pg.h
@@ -545,6 +545,7 @@ public:
     ObjectContextRef obc,
     const std::error_code e,
     ceph_tid_t rep_tid);
+  seastar::future<> clear_ondisk_temp_objs();
 
 private:
 

src/crimson/osd/pg_shard_manager.cc
@@ -35,8 +35,10 @@ seastar::future<> PGShardManager::load_pgs(crimson::os::FuturizedStore& store)
 		pgid
 	      ).then([pgid, &per_shard_state](auto &&pg) {
 		logger().info("load_pgs: loaded {}", pgid);
-		per_shard_state.pg_map.pg_loaded(pgid, std::move(pg));
-		return seastar::now();
+		return pg->clear_ondisk_temp_objs(
+		).then([&per_shard_state, pg, pgid] {
+		  per_shard_state.pg_map.pg_loaded(pgid, std::move(pg));
+		});
 	      });
 	    });
           });

anshuman-agarwala/ceph-ci

src/crimson/osd/pg.h
@@ -603,6 +603,7 @@ public:
     ObjectContextRef obc,
     const std::error_code e,
     ceph_tid_t rep_tid);
+  seastar::future<> clear_temp_objects();
 
 private:
 

src/crimson/osd/pg_shard_manager.cc
@@ -35,8 +35,10 @@ seastar::future<> PGShardManager::load_pgs(crimson::os::FuturizedStore& store)
 		pgid
 	      ).then([pgid, &per_shard_state](auto &&pg) {
 		logger().info("load_pgs: loaded {}", pgid);
-		per_shard_state.pg_map.pg_loaded(pgid, std::move(pg));
-		return seastar::now();
+		return pg->clear_temp_objects(
+		).then([&per_shard_state, pg, pgid] {
+		  per_shard_state.pg_map.pg_loaded(pgid, std::move(pg));
+		});
 	      });
 	    });
           });