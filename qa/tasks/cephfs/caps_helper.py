2b5ed3474cb718e5c907e1f2e515420d96c960b1
Xiubo Li <xiubli@redhat.com>
2024-08-13 16:20:55 +0800

    Revert "qa: use its own open background code"
    This reverts commit d317d2a49e1d6bfe3a78b06b33f0e3ac715581f3.

--- qa/tasks/cephfs/caps_helper.py
+++ qa/tasks/cephfs/caps_helper.py
@@ -1,10 +1,7 @@
 """
 Helper methods to test that MON and MDS caps are enforced properly.
 """
-import os
 import logging
-
-from textwrap import dedent
 
 from tasks.cephfs.cephfs_test_case import CephFSTestCase
 
@@ -94,35 +91,20 @@ class CapsHelper(CephFSTestCase):
         cmdargs += _cmdargs
 
         for mount in mounts:
-            for path, data in zip(filepaths, filedata):
-                if path.find(mount.hostfs_mntpt) != -1:
-                    log.info(f'test absence of {_cmdargs[0]} perm: expect failure {path}.')
+            for path in filepaths:
+                log.info(f'test absence of {_cmdargs[0]} perm: expect failure {path}.')
 
-                    # open the file and hold it. The MDS will issue CEPH_CAP_EXCL_*
-                    # to mount
+                # open the file and hold it. The MDS will issue CEPH_CAP_EXCL_*
+                # to mount
+                proc = mount.open_background(path)
 
-                    pyscript = dedent(f"""
-                        import time
+                cmdargs.append(path)
+                mount.negtestcmd(args=cmdargs, retval=1, errmsgs=possible_errmsgs)
+                cmdargs.pop(-1)
 
-                        with open("{path}", 'w') as f:
-                            f.truncate(0)
-                            f.write("{data}")
-                            f.flush()
-                            while True:
-                                time.sleep(1)
-                        """)
+                mount._kill_background(proc)
 
-                    rproc = mount._run_python(pyscript)
-
-                    mount.wait_for_visible(path, size=len(data))
-
-                    cmdargs.append(path)
-                    mount.negtestcmd(args=cmdargs, retval=1, errmsgs=possible_errmsgs)
-                    cmdargs.pop(-1)
-
-                    mount._kill_background(proc)
-
-                    log.info(f'absence of {_cmdargs[0]} perm was tested successfully')
+                log.info(f'absence of {_cmdargs[0]} perm was tested successfully')
 
     def conduct_neg_test_for_chown_caps(self, filepaths, filedata, mounts, sudo_write=True):
         # flip ownership to nobody. assumption: nobody's id is 65534
