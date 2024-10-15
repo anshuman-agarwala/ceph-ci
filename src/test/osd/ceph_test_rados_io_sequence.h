ac252d196436cabad126887042cd65b1f8a48216
JonBailey1993 <jonathan.bailey1@ibm.com>
2024-10-15 16:00:33 +0100

    common/io_exerciser: Add simple sequences for testing error injects
    Add sequences to test IOs with simple error injects, along with some small fixes for previous error inject implementation.
    
    Signed-off-by: Jon Bailey <jonathan.bailey1@ibm.com>

--- src/test/osd/ceph_test_rados_io_sequence.h
+++ src/test/osd/ceph_test_rados_io_sequence.h
@@ -250,6 +250,8 @@ namespace ceph
       bool get_allow_pool_balancer() { return allow_pool_balancer; }
       bool get_allow_pool_deep_scrubbing() { return allow_pool_deep_scrubbing; }
       bool get_allow_pool_scrubbing() { return allow_pool_scrubbing; }
+      int getChosenK() const { return k; }
+      int getChosenM() const { return m; }
 
     private:
       void create_pool(librados::Rados& rados,
@@ -265,7 +267,9 @@ namespace ceph
       bool allow_pool_balancer;
       bool allow_pool_deep_scrubbing;
       bool allow_pool_scrubbing;
-      
+      int k;
+      int m;
+
       SelectErasureKM skm;
       SelectErasurePlugin spl;
       SelectErasureChunkSize scs;
@@ -287,7 +291,8 @@ namespace ceph
                   ceph::condition_variable& cond,
                   bool dryrun,
                   bool verbose,
-                  std::optional<int>  seqseed);
+                  std::optional<int>  seqseed,
+                  bool testRecovery);
       
       int get_num_io();
       bool readyForIo();
@@ -306,6 +311,9 @@ namespace ceph
       ceph::util::random_number_generator<int>& rng;
       bool verbose;
       std::optional<int> seqseed;
+      int poolK;
+      int poolM;
+      bool testrecovery;
     };
 
     class TestRunner
@@ -341,6 +349,8 @@ namespace ceph
       std::optional<int> seqseed;
       bool interactive;
 
+      bool testrecovery;
+
       bool allow_pool_autoscaling;
       bool allow_pool_balancer;
       bool allow_pool_deep_scrubbing;
@@ -367,7 +377,7 @@ namespace ceph
       bool run_interactive_test();
 
       void help();
-      void list_sequence();
+      void list_sequence(bool testrecovery);
     };
   }
 }
