e201c738152a59d2ab267fe8a96428e7438132d2
Alex Ainscow <aainscow@uk.ibm.com>
2024-11-01 11:24:17 +0000

    V

--- src/osd/OSD.cc
+++ src/osd/OSD.cc
@@ -11016,7 +11016,7 @@ OSDShard::OSDShard(
       cct, osd->whoami, osd->num_shards, id, osd->store->is_rotational(),
       osd->store->get_type(), osd_op_queue, osd_op_queue_cut_off, osd->monc)),
     context_queue(sdata_wait_lock, sdata_cond),
-    ec_extent_cache_lru(10*1024*1024) // 10MiB extent cache
+    ec_extent_cache_lru(4*1024) // FIXME: Trivially small
 {
   dout(0) << "using op scheduler " << *scheduler << dendl;
 }
