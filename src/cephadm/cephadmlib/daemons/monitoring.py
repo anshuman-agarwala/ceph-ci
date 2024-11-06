ceph/ceph

cephadm: Modify the structure of the default container images
Fixes: https://tracker.ceph.com/issues/68605?next_issue_id=68599

Signed-off-by: Shweta Bhosale <Shweta.Bhosale1@ibm.com>

src/cephadm/cephadmlib/daemons/monitoring.py
@@ -3,14 +3,7 @@ import os
 from typing import Dict, List, Tuple
 
 from ..call_wrappers import call, CallVerbosity
-from ceph.cephadm.images import (
-    DEFAULT_ALERT_MANAGER_IMAGE,
-    DEFAULT_GRAFANA_IMAGE,
-    DEFAULT_LOKI_IMAGE,
-    DEFAULT_NODE_EXPORTER_IMAGE,
-    DEFAULT_PROMETHEUS_IMAGE,
-    DEFAULT_PROMTAIL_IMAGE,
-)
+from ceph.cephadm.images import DefaultImages
 from ..constants import (
     UID_NOBODY,
     GID_NOGROUP,
@@ -43,7 +36,7 @@ class Monitoring(ContainerDaemonForm):
 
     components = {
         'prometheus': {
-            'image': DEFAULT_PROMETHEUS_IMAGE,
+            'image': DefaultImages.PROMETHEUS.value.name,
             'cpus': '2',
             'memory': '4GB',
             'args': [
@@ -55,7 +48,7 @@ class Monitoring(ContainerDaemonForm):
             ],
         },
         'loki': {
-            'image': DEFAULT_LOKI_IMAGE,
+            'image': DefaultImages.LOKI.value.name,
             'cpus': '1',
             'memory': '1GB',
             'args': [
@@ -64,7 +57,7 @@ class Monitoring(ContainerDaemonForm):
             'config-json-files': ['loki.yml'],
         },
         'promtail': {
-            'image': DEFAULT_PROMTAIL_IMAGE,
+            'image': DefaultImages.PROMTAIL.value.name,
             'cpus': '1',
             'memory': '1GB',
             'args': [
@@ -75,13 +68,13 @@ class Monitoring(ContainerDaemonForm):
             ],
         },
         'node-exporter': {
-            'image': DEFAULT_NODE_EXPORTER_IMAGE,
+            'image': DefaultImages.NODE_EXPORTER.value.name,
             'cpus': '1',
             'memory': '1GB',
             'args': ['--no-collector.timex'],
         },
         'grafana': {
-            'image': DEFAULT_GRAFANA_IMAGE,
+            'image': DefaultImages.GRAFANA.value.name,
             'cpus': '2',
             'memory': '4GB',
             'args': [],
@@ -93,7 +86,7 @@ class Monitoring(ContainerDaemonForm):
             ],
         },
         'alertmanager': {
-            'image': DEFAULT_ALERT_MANAGER_IMAGE,
+            'image': DefaultImages.ALERT_MANAGER.value.name,
             'cpus': '2',
             'memory': '2GB',
             'args': [

anshuman-agarwala/ceph-ci

src/cephadm/cephadmlib/daemons/monitoring.py
@@ -3,14 +3,7 @@ import os
 from typing import Dict, List, Tuple
 
 from ..call_wrappers import call, CallVerbosity
-from ceph.cephadm.images import (
-    DEFAULT_ALERTMANAGER_IMAGE,
-    DEFAULT_GRAFANA_IMAGE,
-    DEFAULT_LOKI_IMAGE,
-    DEFAULT_NODE_EXPORTER_IMAGE,
-    DEFAULT_PROMETHEUS_IMAGE,
-    DEFAULT_PROMTAIL_IMAGE,
-)
+from ceph.cephadm.images import DefaultImages
 from ..constants import (
     UID_NOBODY,
     GID_NOGROUP,
@@ -43,7 +36,7 @@ class Monitoring(ContainerDaemonForm):
 
     components = {
         'prometheus': {
-            'image': DEFAULT_PROMETHEUS_IMAGE,
+            'image': DefaultImages.PROMETHEUS.value.name,
             'cpus': '2',
             'memory': '4GB',
             'args': [
@@ -55,7 +48,7 @@ class Monitoring(ContainerDaemonForm):
             ],
         },
         'loki': {
-            'image': DEFAULT_LOKI_IMAGE,
+            'image': DefaultImages.LOKI.value.name,
             'cpus': '1',
             'memory': '1GB',
             'args': [
@@ -64,7 +57,7 @@ class Monitoring(ContainerDaemonForm):
             'config-json-files': ['loki.yml'],
         },
         'promtail': {
-            'image': DEFAULT_PROMTAIL_IMAGE,
+            'image': DefaultImages.PROMTAIL.value.name,
             'cpus': '1',
             'memory': '1GB',
             'args': [
@@ -75,13 +68,13 @@ class Monitoring(ContainerDaemonForm):
             ],
         },
         'node-exporter': {
-            'image': DEFAULT_NODE_EXPORTER_IMAGE,
+            'image': DefaultImages.NODE_EXPORTER.value.name,
             'cpus': '1',
             'memory': '1GB',
             'args': ['--no-collector.timex'],
         },
         'grafana': {
-            'image': DEFAULT_GRAFANA_IMAGE,
+            'image': DefaultImages.GRAFANA.value.name,
             'cpus': '2',
             'memory': '4GB',
             'args': [],
@@ -93,7 +86,7 @@ class Monitoring(ContainerDaemonForm):
             ],
         },
         'alertmanager': {
-            'image': DEFAULT_ALERTMANAGER_IMAGE,
+            'image': DefaultImages.ALERTMANAGER.value.name,
             'cpus': '2',
             'memory': '2GB',
             'args': [