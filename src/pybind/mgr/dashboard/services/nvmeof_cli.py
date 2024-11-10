b714368255527e0a16942e93c2dab836944662ee
Tomer Haskalovitch <il033030@Tomers-MBP.lan>
2024-11-10 14:57:56 +0200

    add tomer test3

--- src/pybind/mgr/dashboard/services/nvmeof_cli.py
+++ src/pybind/mgr/dashboard/services/nvmeof_cli.py
@@ -7,6 +7,8 @@ from mgr_module import CLICheckNonemptyFileInput, CLIReadCommand, CLIWriteComman
 from ..rest_client import RequestException
 from .nvmeof_conf import ManagedByOrchestratorException, \
     NvmeofGatewayAlreadyExists, NvmeofGatewaysConfig
+from ..services.nvmeof_client import NVMeoFClient
+
 
 try:
     from ..controllers.nvmeof import NVMeoFSubsystem
@@ -20,6 +22,13 @@ except:
     @CLIReadCommand('dashboard tomer-test-exception-caught')
     def test123(_, gw_group):
         return 0, json.dumps({'a':1})
+
+
+@CLIReadCommand('dashboard tomer-test3')
+def list_nvmeof_gateways2(_, gw_group):
+    return NVMeoFClient(gw_group=gw_group).stub.get_gateway_info(
+                NVMeoFClient.pb2.get_gateway_info_req()
+            )
 
 @CLIReadCommand('dashboard tomer-test')
 def list_nvmeof_gateways2(_):
