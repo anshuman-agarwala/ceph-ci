c1e87c8e75eaf354b9eb57abbba412cf39213193
Jos Collin <jcollin@redhat.com>
2024-11-20 12:42:19 +0530

    qa: add test checking 'journal import' from empty dump file
    Fixes: https://tracker.ceph.com/issues/68928
    Signed-off-by: Jos Collin <jcollin@redhat.com>

--- qa/tasks/cephfs/test_journal_repair.py
+++ qa/tasks/cephfs/test_journal_repair.py
@@ -7,6 +7,7 @@ import json
 import logging
 from textwrap import dedent
 import time
+import tempfile
 
 from teuthology.exceptions import CommandFailedError, ConnectionLostError
 from tasks.cephfs.filesystem import ObjectNotFound, ROOT_INO
@@ -403,3 +404,12 @@ class TestJournalRepair(CephFSTestCase):
             "timeout": "1h"
         })
 
+    def test_import_from_empty_file(self):
+        """
+        That the 'journal import' recognizes empty file read and errors out.
+        """
+        self.fs.fail()
+        with tempfile.NamedTemporaryFile() as temp:
+            output = self.fs.journal_tool(["journal", "import", temp.name], 0)
+            if f'Error reading {temp}' not in output:
+                raise RuntimeError(f"Unexpected journal-tool result: '{output}'")
