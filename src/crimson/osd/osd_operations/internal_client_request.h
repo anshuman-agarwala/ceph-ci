athanatos/ceph

crimson: convert client_request, internal_client_request, snaptrim_event to use obc stages
Signed-off-by: Samuel Just <sjust@redhat.com>

src/crimson/osd/osd_operations/internal_client_request.h
@@ -4,6 +4,7 @@
 #pragma once
 
 #include "crimson/common/type_helpers.h"
+#include "crimson/osd/object_context_loader.h"
 #include "crimson/osd/osd_operation.h"
 #include "crimson/osd/osd_operations/client_request_common.h"
 #include "crimson/osd/pg.h"
@@ -48,6 +49,7 @@ private:
   Ref<PG> pg;
   epoch_t start_epoch;
   OpInfo op_info;
+  std::optional<ObjectContextLoader::Manager> obc_manager;
   PipelineHandle handle;
 
 public:
@@ -55,12 +57,8 @@ public:
 
   std::tuple<
     StartEvent,
-    CommonPGPipeline::WaitForActive::BlockingEvent,
-    PGActivationBlocker::BlockingEvent,
-    CommonPGPipeline::RecoverMissing::BlockingEvent,
-    CommonPGPipeline::CheckAlreadyCompleteGetObc::BlockingEvent,
-    CommonPGPipeline::LockOBC::BlockingEvent,
-    CommonPGPipeline::Process::BlockingEvent,
+    CommonOBCPipeline::Process::BlockingEvent,
+    CommonOBCPipeline::WaitRepop::BlockingEvent,
     CompletionEvent
   > tracking_events;
 };

src/crimson/osd/osd_operations/snaptrim_event.h
@@ -6,6 +6,7 @@
 #include <iostream>
 #include <seastar/core/future.hh>
 
+#include "crimson/osd/object_context_loader.h"
 #include "crimson/osd/osdmap_gate.h"
 #include "crimson/osd/osd_operation.h"
 #include "crimson/common/subop_blocker.h"
@@ -154,6 +155,7 @@ private:
   }
 
   Ref<PG> pg;
+  std::optional<ObjectContextLoader::Manager> obc_manager;
   PipelineHandle handle;
   osd_op_params_t osd_op_p;
   const hobject_t coid;
@@ -165,9 +167,8 @@ public:
 
   std::tuple<
     StartEvent,
-    CommonPGPipeline::CheckAlreadyCompleteGetObc::BlockingEvent,
-    CommonPGPipeline::Process::BlockingEvent,
-    CommonPGPipeline::WaitRepop::BlockingEvent,
+    CommonOBCPipeline::Process::BlockingEvent,
+    CommonOBCPipeline::WaitRepop::BlockingEvent,
     CompletionEvent
   > tracking_events;
 };

anshuman-agarwala/ceph-ci

src/crimson/osd/osd_operations/internal_client_request.h
@@ -4,6 +4,7 @@
 #pragma once
 
 #include "crimson/common/type_helpers.h"
+#include "crimson/osd/object_context_loader.h"
 #include "crimson/osd/osd_operation.h"
 #include "crimson/osd/osd_operations/client_request_common.h"
 #include "crimson/osd/pg.h"
@@ -48,6 +49,7 @@ private:
   Ref<PG> pg;
   epoch_t start_epoch;
   OpInfo op_info;
+  std::optional<ObjectContextLoader::Orderer> obc_orderer;
   PipelineHandle handle;
 
 public:
@@ -55,12 +57,8 @@ public:
 
   std::tuple<
     StartEvent,
-    CommonPGPipeline::WaitForActive::BlockingEvent,
-    PGActivationBlocker::BlockingEvent,
-    CommonPGPipeline::RecoverMissing::BlockingEvent,
-    CommonPGPipeline::CheckAlreadyCompleteGetObc::BlockingEvent,
-    CommonPGPipeline::LockOBC::BlockingEvent,
-    CommonPGPipeline::Process::BlockingEvent,
+    CommonOBCPipeline::Process::BlockingEvent,
+    CommonOBCPipeline::WaitRepop::BlockingEvent,
     CompletionEvent
   > tracking_events;
 };

src/crimson/osd/osd_operations/snaptrim_event.h
@@ -6,6 +6,7 @@
 #include <iostream>
 #include <seastar/core/future.hh>
 
+#include "crimson/osd/object_context_loader.h"
 #include "crimson/osd/osdmap_gate.h"
 #include "crimson/osd/osd_operation.h"
 #include "crimson/common/subop_blocker.h"
@@ -154,6 +155,7 @@ private:
   }
 
   Ref<PG> pg;
+  std::optional<ObjectContextLoader::Orderer> obc_orderer;
   PipelineHandle handle;
   osd_op_params_t osd_op_p;
   const hobject_t coid;
@@ -165,9 +167,8 @@ public:
 
   std::tuple<
     StartEvent,
-    CommonPGPipeline::CheckAlreadyCompleteGetObc::BlockingEvent,
-    CommonPGPipeline::Process::BlockingEvent,
-    CommonPGPipeline::WaitRepop::BlockingEvent,
+    CommonOBCPipeline::Process::BlockingEvent,
+    CommonOBCPipeline::WaitRepop::BlockingEvent,
     CompletionEvent
   > tracking_events;
 };