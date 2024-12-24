xxhdx1985126/ceph

crimson/osd/pg_shard_manager: discard outdated operations when the corresponding pgs are already removed
Fixes: https://tracker.ceph.com/issues/68286
Signed-off-by: Xuehan Xu <xuxuehan@qianxin.com>

src/crimson/osd/osd_operations/logmissing_request.h
@@ -36,6 +36,9 @@ public:
   }
   PipelineHandle &get_handle() { return handle; }
   epoch_t get_epoch() const { return req->get_min_epoch(); }
+  epoch_t get_map_epoch() const {
+    return req->get_map_epoch();
+  }
 
   ConnectionPipeline &get_connection_pipeline();
 

src/crimson/osd/osd_operations/peering_event.h
@@ -53,6 +53,10 @@ protected:
   float delay = 0;
   PGPeeringEvent evt;
 
+  epoch_t get_map_epoch() const {
+    return evt.get_epoch_sent();
+  }
+
   const pg_shard_t get_from() const {
     return from;
   }
@@ -92,6 +96,10 @@ public:
     delay(delay),
     evt(std::forward<Args>(args)...)
   {}
+
+  bool requires_pg() const final {
+    return evt.requires_pg;
+  }
 
   void print(std::ostream &) const final;
   void dump_detail(ceph::Formatter* f) const final;

src/crimson/osd/osd_operations/pg_advance_map.h
@@ -50,6 +50,10 @@ public:
     PGPeeringPipeline::Process::BlockingEvent
   > tracking_events;
 
+  epoch_t get_map_epoch() const {
+    return to;
+  }
+
 private:
   PGPeeringPipeline &peering_pp(PG &pg);
 };

src/crimson/osd/osd_operations/recovery_subrequest.h
@@ -39,6 +39,9 @@ public:
   }
   PipelineHandle &get_handle() { return handle; }
   epoch_t get_epoch() const { return m->get_min_epoch(); }
+  epoch_t get_map_epoch() const {
+    return m->get_map_epoch();
+  }
 
   ConnectionPipeline &get_connection_pipeline();
 

anshuman-agarwala/ceph-ci

(cherry picked from commit 1f1051d91f320636f513faeb2d113542bcb9e5c3)

src/crimson/osd/osd_operations/logmissing_request.h
@@ -36,6 +36,9 @@ public:
   }
   PipelineHandle &get_handle() { return handle; }
   epoch_t get_epoch() const { return req->get_min_epoch(); }
+  epoch_t get_epoch_sent_at() const {
+    return req->get_map_epoch();
+  }
 
   ConnectionPipeline &get_connection_pipeline();
 

src/crimson/osd/osd_operations/peering_event.h
@@ -44,6 +44,10 @@ protected:
   float delay = 0;
   PGPeeringEvent evt;
 
+  epoch_t get_epoch_sent_at() const {
+    return evt.get_epoch_sent();
+  }
+
   const pg_shard_t get_from() const {
     return from;
   }
@@ -83,6 +87,10 @@ public:
     delay(delay),
     evt(std::forward<Args>(args)...)
   {}
+
+  bool requires_pg() const final {
+    return evt.requires_pg;
+  }
 
   void print(std::ostream &) const final;
   void dump_detail(ceph::Formatter* f) const final;

src/crimson/osd/osd_operations/pg_advance_map.h
@@ -50,6 +50,10 @@ public:
     PGPeeringPipeline::Process::BlockingEvent
   > tracking_events;
 
+  epoch_t get_epoch_sent_at() const {
+    return to;
+  }
+
 private:
   PGPeeringPipeline &peering_pp(PG &pg);
 };

src/crimson/osd/osd_operations/recovery_subrequest.h
@@ -39,6 +39,9 @@ public:
   }
   PipelineHandle &get_handle() { return handle; }
   epoch_t get_epoch() const { return m->get_min_epoch(); }
+  epoch_t get_epoch_sent_at() const {
+    return m->get_map_epoch();
+  }
 
   ConnectionPipeline &get_connection_pipeline();
 