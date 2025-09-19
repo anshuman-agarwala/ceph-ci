60d571ce39aaabe9655184024a1ef6c1c3a2f1dd
Anshuman Agarwala <anshu0204@gmail.com>
2025-09-19 14:49:33 +0530

    Update blogbench.sh
    Signed-off-by: Anshuman Agarwala <anshu0204@gmail.com>

--- qa/workunits/suites/blogbench.sh
+++ qa/workunits/suites/blogbench.sh
@@ -2,10 +2,10 @@
 set -ex
 
 echo "getting blogbench"
-wget http://download.ceph.com/qa/blogbench-1.0.tar.bz2
+wget https://github.com/jedisct1/Blogbench/releases/download/1.2/blogbench-1.2.tar.bz2
 #cp /home/gregf/src/blogbench-1.0.tar.bz2 .
-tar -xvf blogbench-1.0.tar.bz2
-cd blogbench-1.0/
+tar -xvf blogbench-1.2.tar.bz2
+cd blogbench-1.2/
 echo "making blogbench"
 ./configure
 make
